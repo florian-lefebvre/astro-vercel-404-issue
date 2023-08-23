046f1260303782a0024e74915b995134bd8ca765
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-08-23 10:16:18 +0200

    feat: add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-vercel-404-issue.vercel.app",
+  base: "/",
+  trailingSlash: "never",
+  output: "hybrid",
+  compressHTML: true,
+  adapter: vercel({}),
+});
